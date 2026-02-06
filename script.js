2cd5f66eb2cf88d56e5c84e6f2d7d36fd6e269d9
irisbaltus <150801536+irisbaltus@users.noreply.github.com>
2026-02-06 14:57:22 +0100

    scroll driven kleurverloop

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
 document.startViewTransition(() => {
 	filterItems();
 	});
+
+insertHeaderInfo()
+
+async function insertHeaderInfo(){
+	const url = "https://fdnd.directus.app/items/person/291";
+
+
+
+let response = await fetch(url)
+	console.log(response)
+
+let headerNaam = await response.json()
+	console.log(headerNaam)
+
+let moneStudio = headerNaam.data
+
+	let characterHTML =
+	`<header>
+		<h1> ${moneStudio.nickname}  </h1>    
+    </header>`
+
+document.body.insertAdjacentHTML("afterbegin", characterHTML)
+}
